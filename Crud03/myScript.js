317c94c6238ce5e9cb9f71877974c85e7facbf6d
Michel Naoki <lojachimbinha@gmail.com>
2023-11-18 20:18:39 -0300

    add mult td

--- Crud03/myScript.js
+++ Crud03/myScript.js
@@ -1,27 +1,29 @@
 let button = document.getElementById('id_button');
 button.addEventListener('click', cadastrar);
 let form = document.getElementById('form');
-let table = document.getElementById('tab_users');
+let table = document.getElementById('id-table');
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
   })
 
 function cadastrar() {
-    let nome = document.getElementById('id_nome').value;
-    let birthdate = document.getElementById('id_nascimento').value;
-    let data = new Date(birthdate);
-    let dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+    let $nome = document.getElementById('id_nome').value;
+    let $nasc = document.getElementById('id_nascimento').value;
+    let newNac = new Date($nasc);
+    let data = newNac.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+
     let usuario = {
-        name: nome,
-        dataNascimento: dataFormatada    
-    }
-    localStorage.setItem('user', JSON.stringify(usuario));
-    let tr = document.createElement('tr');
-    table.appendChild(tr);
-    tr.id = 'duran';
-    let trow = document.getElementById('duran');
-    trow.innerHTML = `
-    <td class="tdName">${JSON.parse(localStorage.getItem('user')).name}</td>
-    <td class="tdBirthdate">${JSON.parse(localStorage.getItem('user')).dataNascimento}</td>`;
+        name: $nome,
+        nascimento: data  
+    };
+
+    let arrayPessoas = JSON.parse(localStorage.getItem('user')) || [];
+    arrayPessoas.push(usuario);
+    localStorage.setItem('user', JSON.stringify(arrayPessoas));
+
+    let linha = document.createElement('tr');
+    linha.setAttribute('id', 'id-linha');
+    document.getElementById('id-table').appendChild(linha);
+    linha.innerHTML = `<td>${usuario.name}</td><td>${usuario.nascimento}</td><td><button onclick="">Editar</button></td>`;
 }
